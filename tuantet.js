a2b7737f2c5f3b2482ea0bc96a550ed88f3010f1
thienkieunu6-lab <thienkieunu6@gmail.com>
2026-02-27 13:25:29 +0000

    add index html for github pages

---
+++ tuantet.js
@@ -0,0 +1,4 @@
+document.querySelector('.contact-form')?.addEventListener('submit', (event) => {
+  event.preventDefault();
+  alert('Cảm ơn bạn đã đóng góp ý kiến cho website Tết quê hương!');
+});
